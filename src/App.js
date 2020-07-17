86f2bf834fad375b9f56e928bfd550636054d211
Eduardo Augusto <eduardoaugusto2016880@gmail.com>
2020-07-16 21:43:11 -0300

    Adicionando o Navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-
+import Navbar from './components/navbar';
 
 export default function App() {
   return (
-    <div classNameNameName="App">
-      <div className="alert alert-dismissible alert-warning">
-        <button type="button" className="close" data-dismiss="alert">&times;</button>
-        <h4 className="alert-heading">Warning!</h4>
-        <p className="mb-0">Hello <a href="#" className="alert-link">Bootswatch</a>.</p>
-      </div>
-    </div>
+    <>
+  <Navbar/>
+    </>
   );
 }
